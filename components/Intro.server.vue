c49d11058048159d8d83aa7c44a74dc7b5be49e4
Ilko Valchev <ilko@Ilkos-MacBook-Pro.local>
2024-10-18 09:22:39 +0300

    set up

---
+++ components/Intro.server.vue
@@ -0,0 +1,7 @@
+<template>
+    <img src="/img/dogs.png" />
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <Intro />
+    </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
